91d1fd40d03bcfaad56e08c6e5f0f1fd018c4937
evankerkore_decideom <evankerkore@decideom.fr>
2023-05-24 09:12:10 +0200

    On archive les sources en même temps qu'on les importe

--- Code/importer_sources.py
+++ Code/importer_sources.py
@@ -6,12 +6,15 @@ This is a temporary script file.
 """
 
 import pandas
+import shutil
+import datetime
 
 path_googlesheet = "C:\\GitHub\\Pluie_Locale\\Secrets\\GoogleSheetID.txt"
 transco_filename = "transco_user_personne.csv"
 
 fait_entete_csv = "id_personne;id_temporalite;valeur\n"
 folder_input = "C:\\GitHub\\Pluie_Locale\\Data\\Sources\\"
+archive_folder_output = "C:\\GitHub\\Pluie_Locale\\Data\\Sources\\Archives\\"
 folder_output = "C:\\GitHub\\Pluie_Locale\\Data\\Cibles\\"
 
 def get_transco_in_df(path_input):
@@ -26,6 +29,12 @@ def get_secret(path):
     f.close()
     return chaine
 
+def copy_and_archive(folder_in, folder_out, name):
+    file_archived = name + "_" + datetime.datetime.now().strftime('%Y%m%d_%H%M%S') + ".csv"
+    file_in = folder_in + name + ".csv"
+    file_out = folder_out + file_archived
+    shutil.copy(file_in , file_out)
+
 googleSheetId = get_secret(path_googlesheet)
     
 transco_df = get_transco_in_df(folder_output + transco_filename)
@@ -36,3 +45,4 @@ for onglet in get_liste_onglet(transco_df):
     	onglet)
     data_df = pandas.read_csv(url)[["date", "valeur"]]
     data_df.to_csv(folder_input + onglet + ".csv", sep=";", index=False)
+    copy_and_archive(folder_input, archive_folder_output, onglet)
