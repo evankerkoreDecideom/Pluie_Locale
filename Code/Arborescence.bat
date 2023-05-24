519ba85720977a861fdd9d6411a0a3a0475bba11
evankerkore_decideom <evankerkore@decideom.fr>
2023-05-24 09:10:20 +0200

    suppression du fichier car on ne passe plus par un .bat

--- Code/Arborescence.bat
+++
@@ -1,6 +0,0 @@
-mkdir C:\\GitHub
-mkdir C:\\GitHub\\Pluie_Locale
-mkdir C:\\GitHub\\Pluie_Locale\\Secrets
-mkdir C:\\GitHub\\Pluie_Locale\\Data
-mkdir C:\\GitHub\\Pluie_Locale\\Data\\Sources
-mkdir C:\\GitHub\\Pluie_Locale\\Data\\Cibles
