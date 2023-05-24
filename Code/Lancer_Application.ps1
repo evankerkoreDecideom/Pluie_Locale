283250325eb246af13a1c66d715572b29b156002
evankerkore_decideom <evankerkore@decideom.fr>
2023-05-24 11:00:58 +0200

    Ajout code source qui MAJ et lance le rapport

---
+++ Code/Lancer_Application.ps1
@@ -0,0 +1,5 @@
+﻿cd C:\GitHub\Pluie_Locale\
+git pull
+C:\GitHub\Pluie_Locale\Code\dist\importer_sources.exe
+C:\GitHub\Pluie_Locale\Code\dist\fait.exe
+C:\GitHub\Pluie_Locale\Rapport\Pluie_Locale.pbix
