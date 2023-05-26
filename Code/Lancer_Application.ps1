cfe3690f5b93f9a66884d09302969e323c3d8679
evankerkore_decideom <evankerkore@decideom.fr>
2023-05-26 14:10:48 +0200

    On est passé sur de Powershell à Python car moins contraignant

--- Code/Lancer_Application.ps1
+++
@@ -1,5 +0,0 @@
-﻿cd C:\GitHub\Pluie_Locale\
-git pull
-C:\GitHub\Pluie_Locale\Code\dist\importer_sources.exe
-C:\GitHub\Pluie_Locale\Code\dist\fait.exe
-C:\GitHub\Pluie_Locale\Rapport\Pluie_Locale.pbix
